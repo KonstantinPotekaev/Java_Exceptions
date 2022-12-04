c6a440f01b516aa247220a3a04162f000c27af20
Константин Потекаев <kostya.potekaev@gmail.com>
2022-12-04 13:52:34 +0300

    Initial commit

--- Lesson_2/src/Task1.java
+++ Lesson_2/src/Task1.java
@@ -1,2 +1,18 @@
+﻿import java.util.Scanner;
+
 public class Task1 {
+    public static void main(String[] args) {
+        System.out.print("Введите число типа float: ");
+        while(true){
+            try{
+                Scanner in= new Scanner(System.in);
+                var a = Float.parseFloat(in.next());
+                System.out.println(a);
+                break;
+            }
+            catch(Exception ex){
+                System.out.print("Введите число типа float: ");
+            }
+        }
+    }
 }
